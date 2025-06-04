eff1dfa9b074c323bf7f1c960d371ad4a307c9a3
doxcom <aldoperez1319@gmail.com>
2025-06-03 20:30:01 -0600

    adding zip files with functions and postman collection to test it

--- LambdaWithLayer/create_zip.sh
+++ LambdaWithLayer/create_zip.sh
@@ -7,4 +7,22 @@ zip -r get.zip index.mjs
 mv get.zip ../../
 cd ../..
 
+echo "Creating zip for POST Function"
+cd LambdaFunctionsWithLayer/post
+zip -r post.zip index.mjs
+mv post.zip ../../
+cd ../..
+
+echo "Creating zip for UPDATE Function"
+cd LambdaFunctionsWithLayer/update
+zip -r update.zip index.mjs
+mv update.zip ../../
+cd ../..
+
+echo "Creating zip for DELETE Function"
+cd LambdaFunctionsWithLayer/delete
+zip -r delete.zip index.mjs
+mv delete.zip ../../
+cd ../..
 echo "Success!"
+
